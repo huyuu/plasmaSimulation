51f0a9e3c862986a425a65db7e077c9cb6fe7bfc
yuyang <huyuu258@gmail.com>
2020-12-23 14:56:41 +0900

    0.19

--- motionSimulation.py
+++ motionSimulation.py
@@ -53,8 +53,8 @@ class Simulator():
             x0_z = envHigh.plotCubeZ0
             v0_x = envHigh.plotCubeX0/1e2 * (2*nu.random.rand()-1)
             v0_y = envHigh.plotCubeY0/1e2 * (2*nu.random.rand()-1)
-            v0_z = envHigh.plotCubeZ0 * (-1)*(nu.random.rand()+0.5)
-            # v0_z = -100.0
+            # v0_z = envHigh.plotCubeZ0 * (-1)*(nu.random.rand()+0.5)
+            v0_z = -10.0
             particlesHigh.append(Particle(mass=1e-6, q=1.0, x0=nu.array([x0_x, x0_y, x0_z]), v0=nu.array([v0_x, v0_y, v0_z]), a0=nu.zeros(3)))
             particlesLow.append(Particle(mass=1e-6, q=1.0, x0=nu.array([x0_x, x0_y, x0_z]), v0=nu.array([v0_x, v0_y, v0_z]), a0=nu.zeros(3)))
         # simulate
@@ -139,7 +139,6 @@ class Simulator():
         ax.quiver(_xs, _ys, _zs, bs_x, bs_y, bs_z, length=length, arrow_length_ratio=arrow_length_ratio)
         # plot trajectories
         for particle in particles:
-            print(particle.spaceTimeSeries)
             ax.scatter3D(particle.spaceTimeSeries[:, 0].ravel(), particle.spaceTimeSeries[:, 1].ravel(), particle.spaceTimeSeries[:, 2].ravel(), c=particle.spaceTimeSeries[:, 3].ravel(), cmap='viridis')
         pl.show()
 
